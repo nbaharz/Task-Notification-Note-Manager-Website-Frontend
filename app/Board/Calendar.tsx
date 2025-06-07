93e39cf7833f463b07554fcd80078443df60ae27
nbaharz <nurbaharyvz@gmail.com>
2025-06-07 14:32:55 +0300

    Add calendar to Board page

---
+++ app/Board/Calendar.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './Calendar.css'
+
+export default function CustomCalendar() {
+  const [date, setDate] = useState<Date | null>(new Date());
+
+  return (
+    <div className="calendar-container">
+      <Calendar
+        onChange={(e) => setDate(e as Date)}
+        value={date}
+        locale="tr-TR"
+        calendarType="gregory"
+        className="custom-calendar"
+      />
+    </div>
+  );
+}
